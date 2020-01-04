1db44f2bd86ffbdd6670aa7478cffa6e899baaa0
karytonn <karytonndev@gamil.com>
2020-01-04 17:24:48 -0300

    implement getLink.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
+function getLink() {
+    let inputnumber = document.querySelector('#inputnumber')
+    let inputmessage = document.querySelector('#inputmessage')
 
+    let linktocopy = document.querySelector('#linktocopy')
+
+    let linkbase = "https://api.whatsapp.com/send?phone=55"
+
+
+    let cellphone = Number(inputnumber.value)
+    let message = inputmessage.value
+
+    console.log(cellphone)
+
+    
+    linkbase += cellphone
+    linkbase += message
+
+    console.log(linkbase)
+
+    linktocopy.innerHTML = linkbase
+
+
+}
+
+console.log("OK")
+
+//https://api.whatsapp.com/send?phone=55629833232423&text=mensagem

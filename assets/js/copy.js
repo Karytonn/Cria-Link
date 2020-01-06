586cf5b82c9c9a17102016505cca3ff0f994380e
karytonn <karytonndev@gamil.com>
2020-01-06 20:53:23 -0300

    update

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -1,6 +1,6 @@
 function copyLink() {
     let link = document.getElementById('linktocopy')
     link.select()
-    document.execCommand("copy")
+    document.execCommand('copy')
     console.log("Copy to clipboard: " + link.value)
   } 

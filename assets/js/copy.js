b28031bf7f3e421c8399a01a8ab7791239fdb847
karytonn <karytonndev@gamil.com>
2020-01-06 21:35:24 -0300

    update

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -14,11 +14,12 @@
 
 async function copyLink() {
     try {
-        const link = document.getElementById('linktocopy')
-        // 1) Copy text
-        await navigator.clipboard.writeText(link.value);
-  
-        // 2) Catch errors
+        let link = document.getElementById('linktocopy')
+            if(link.value.length > 0) {
+                await navigator.clipboard.writeText(link.value);
+            } else {
+                alert("[ERRO] Primeiro gere um link!")
+            }
         } catch (err) {
             console.error('Failed to copy: ', err);
         }

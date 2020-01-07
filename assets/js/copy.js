e277fea67283a96fb04aa4685c1ed35b05845ed3
karytonn <karytonndev@gamil.com>
2020-01-06 21:27:20 -0300

    update

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -1,6 +1,25 @@
-function copyLink() {
-    let link = document.getElementById('linktocopy')
-    link.select()
-    document.execCommand('copy')
-    console.log("Copy to clipboard: " + link.value)
-  } 
+// function copyLink() {
+//     const link = document.getElementById('linktocopy')
+//     link.focus()
+//     link.select()
+
+//     const isSucesseful = document.execCommand('copy')
+
+//     if (!isSucesseful) {
+//         console.error("[ERRO to copy Text.]")
+//     }
+    
+//     console.log("Copy to clipboard: " + link.value)
+//   } 
+
+async function copyLink() {
+    try {
+        const link = document.getElementById('linktocopy')
+        // 1) Copy text
+        await navigator.clipboard.writeText(link.value);
+  
+        // 2) Catch errors
+        } catch (err) {
+            console.error('Failed to copy: ', err);
+        }
+    }
